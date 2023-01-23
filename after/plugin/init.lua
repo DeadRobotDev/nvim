d3d77433fb82b78a10b69c6e4a97b659bc2d7931
DeadRobotDev <99739042+DeadRobotDev@users.noreply.github.com>
2023-01-23 23:53:28 +0000

    feat: undo remap hjkl

--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -6,13 +6,6 @@ local anoremap = keymap.anoremap
 nnoremap('<Leader>pv', '<Cmd>Ex<CR>')
 nnoremap('<CR>', '<Plug>NetrwBrowseX')
 
--- DO NOT remap h until used to jkl;
-nnoremap('h', '<Nop>')
-nnoremap('j', 'h')
-nnoremap('k', 'j')
-nnoremap('l', 'k')
-nnoremap(';', 'l')
-
 anoremap('<Left>', '<Nop>')
 anoremap('<Right>', '<Nop>')
 anoremap('<Up>', '<Nop>')
